0f7a157216afe82f7de37e84c0576c6e1dd41a80
sandaru07 <108501284+sandaru07@users.noreply.github.com>
2023-03-21 08:09:57 -0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -4,13 +4,13 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://github.com/ImYanXiao/Elaina-MultiDevice*`
+let str = `*https://github.com/*`
     let waifu = await fetch('https://raw.githubusercontent.com/ArugaZ/grabbed-results/main/random/anime/waifu.txt')
     let chen = await waifu.text()
     let ran = chen.split('\n')
     let yae = ran[Math.floor(Math.random() * ran.length)]
     let thumb = await(await fetch(yae)).buffer()
-conn.sendButtonDoc(m.chat, str, wm, 'ᴡᴀɴᴛ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ?', 'Thanks','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
+conn.sendButtonDoc(m.chat, str, wm, 'ᴡᴀɴᴛ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ?', 'OK','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
     mediaUrl: global.sig,
     title: wm3,
     body: 'want source code?',
