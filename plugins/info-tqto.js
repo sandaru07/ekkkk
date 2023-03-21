57c1d81e3dfcbafb1d8286cb59569d1b1f2fd9c4
sandaru07 <108501284+sandaru07@users.noreply.github.com>
2023-03-21 08:12:08 -0700

    Update info-tqto.js

--- plugins/info-tqto.js
+++ plugins/info-tqto.js
@@ -12,59 +12,7 @@ let handler = async (m) => {
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
     else who = m.sender
     let user = global.db.data.users[who]
-let tqto = `*▸ - - - —「 BIG THANKS TO 」— - - - ◂*
-*❉ Adiwajshing:*
-https://github.com/adiwajshing
-*❉ Nurutomo:*
-https://github.com/Nurutomo
-*❉ Istikmal:* 
-https://github.com/BochilGaming
-*❉ Ariffb:*
-https://github.com/Ariffb25
-*❉ Ilman:*
-https://github.com/ilmanhdyt
-*❉ Amirul:*
-https://github.com/amiruldev20
-*❉ Rasel:*
-https://github.com/raselcomel
-*❉ Fatur:*
-https://github.com/Ftwrr
-*❉ Rominaru:*
-https://github.com/Rominaru
-*❉ Kannachann:*
-https://github.com/Kannachann
-*❉ The.sad.boy01:*
-https://github.com/kangsad01
-*❉ Ameliascrf:*
-https://github.com/Ameliascrf
-*❉ Fokus ID:*
-https://github.com/Fokusdotid
-*❉ Johannes:*
-https://github.com/Johannes2803
-*❉ BrunoSobrino:*
-https://github.com/BrunoSobrino
-*❉ Krisna:*
-https://github.com/NevtBotz
-*❉ LitRHap:*
-https://github.com/LitRHap
-*❉ Rlxfly:*
-https://github.com/Rlxfly
-*❉ Aine:*
-https://github.com/Aiinne
-*❉ Papah-Chan:*
-https://github.com/FahriAdison
-*▸ - - - —「 Penulis Ulang 」— - - - ◂*
-*❉ ImYanXiao:*
-https://github.com/ImYanXiao
-*▸ - - - —「 Thanks For Help Me 」— - - - ◂*
-*❉ Kannachan:*
-*❉ The.sadboy.01:*
-*❉ Papah-Chan:*
-*❉ Johannes:*
-*❉ Krisna:*
-*❉ Rlxfly:*
-
-`
+let tqto = `Nimesh Oficial AND ME`
 conn.sendButtonDoc(m.chat, tqto, wm, 'ᴛʜᴀɴᴋ ʏᴏᴜ', 'Thanks','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
     mediaUrl: global.sig,
     mediaType: "VIDEO",
